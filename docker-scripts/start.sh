f05fcb0ef837dab1f6e7c25cad84af730c6fb744
murtazaa-hussainn <61433346+murtazaa-hussainn@users.noreply.github.com>
2024-06-05 05:32:02 +0500

    Update start.sh

--- docker-scripts/start.sh
+++ docker-scripts/start.sh
@@ -4,4 +4,4 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
 
 docker pull 058264531795.dkr.ecr.us-east-1.amazonaws.com/sp-backend-app-ecr:latest
 
-docker run --env-file /opt/sp-backend-app-ecr/.env -dit -p 5000:5000 --restart on-failure:5 --name backend 058264531795.dkr.ecr.us-east-1.amazonaws.com/sp-backend-app-ecr:latest
+docker run --env-file /opt/sp-backend-app/.env -dit -p 5000:5000 --restart on-failure:5 --name backend 058264531795.dkr.ecr.us-east-1.amazonaws.com/sp-backend-app-ecr:latest
