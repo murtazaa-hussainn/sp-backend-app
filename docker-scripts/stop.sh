a207cfc5f426bcf257dc0c83afad3a2c1883ab10
murtazaa-hussainn <61433346+murtazaa-hussainn@users.noreply.github.com>
2024-06-05 05:27:02 +0500

    Update stop.sh

--- docker-scripts/stop.sh
+++ docker-scripts/stop.sh
@@ -3,7 +3,7 @@
 if [ $( docker ps -a | grep backend | wc -l ) -gt 0 ]; then
   docker stop backend
   docker rm backend
-  docker rmi 058264531795.dkr.ecr.us-east-1.amazonaws.com/sp-backend-app:latest
+  docker rmi 058264531795.dkr.ecr.us-east-1.amazonaws.com/sp-backend-app-ecr:latest
 fi
 
 exit 0
